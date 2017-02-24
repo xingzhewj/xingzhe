87d338b604d72550f1653f286dc2657b5a34bc6e
wangjie19 <wangjie19@baidu.com>
2017-02-24 22:50:53 +0800

    service work

---
+++ src/common/serviceWork.js
@@ -0,0 +1,16 @@
+/**
+* @file service work脚本
+**/
+
+if (navigator.serviceWorker) {
+    navigator.serviceWorker.register('./test.js', {
+        scope: './'
+    }).then(function (req, res) {
+        console.log(req);
+    }).catch(function (err) {
+        console.log(err);
+    })
+}
+else {
+    document.write('<h1>不支持service work</h1>');
+}
